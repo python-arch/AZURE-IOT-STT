60dddfbf6d25cb40008b4ab4f35375cbd0959f5a
Python8723 <53436138+python-arch@users.noreply.github.com>
2024-05-23 10:58:29 +0300

    update temp

--- app.py
+++ app.py
@@ -14,30 +14,53 @@ device_id = "TND_WH_10521C663374"
 def index():
     return jsonify({'message': 'Connected to the server'})
 
-@app.route('/up')
+@app.route('/update_temp' , methods=['POST'])
 def up():
-    sent_message = "1"
+    data = request.json
+    value = data.get('temp_user', None)
+    
+    data = {
+    "TRANSMISSION_PATH": {
+        "TX": "BACKEND",
+        "RX": "DEVICE",
+        "BACKEND_DATA": {
+            "APP_STATE": "OPEN",
+            "AVERAGE_POWER_WATT": "KEEP",
+            "ACTIVATION": "DONE",
+            "WIFI_SETTING": "KEEP",
+            "APP_DATA": {
+                "MODE_DATA": {
+                    "FEATURES": "TURBO",
+                    "MODE": "COOL"
+                },
+                "COMPONENT_DATA": {
+                    "PLASMA": "ON",
+                    "FAN": "LOW",
+                    "H_LOUVRE": "POS_1",
+                    "V_LOUVRE": "POS_2"
+                },
+                "CONTROL_DATA": {
+                    "TIMER_STATE": "OFF",
+                    "TIMER_HOURS": "0",
+                    "TEMP_CELSIUS_USER": value
+                }
+            }
+        }
+    }
+}
+    
+    sent_message = json.dumps(data)
+
     try:
         registry_manager = IoTHubRegistryManager.from_connection_string(connection_str)
         registry_manager.send_c2d_message(device_id, sent_message)
-        return jsonify({'message': f"Message up sent successfully"})
+        print(sent_message)
+        return jsonify({'message': f"Message {sent_message} sent successfully"})
     except msrest.exceptions.HttpOperationError as ex:
         return jsonify({'error': f"HttpOperationError: {ex.response.text}"})
     except Exception as ex:
         return jsonify({'error': f"Unexpected error: {ex}"})
 
-@app.route('/down')
-def down():
-    sent_message = "0"
-    try:
-        registry_manager = IoTHubRegistryManager.from_connection_string(connection_str)
-        registry_manager.send_c2d_message(device_id, sent_message)
-        return jsonify({'message': f"Message down sent successfully"})
-    except msrest.exceptions.HttpOperationError as ex:
-        return jsonify({'error': f"HttpOperationError: {ex.response.text}"})
-    except Exception as ex:
-        return jsonify({'error': f"Unexpected error: {ex}"})
-    
 def run_flask():
     app.run(debug=True)
 
